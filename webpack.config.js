b550181f0ea9dc582e9b900940aa8f1e1d29e9e9
Anar Agaev <anar.n.agaev@gmail.com>
2019-10-28 12:07:24 +0300

    init project

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        app: './src/js/index.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './dist/js'),
+        publicPath: '/dist/js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, './dist'),
+        compress: true, // Enable gzip compression for everything served
+        overlay: true // Show errors and warnings in the browser vewport
+    }
+};
