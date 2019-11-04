3d1a738a2ad0832fb2a9fcd191aff6254e3e4001
Anar Agaev <anar.n.agaev@gmail.com>
2019-11-04 17:27:16 +0300

    added pug to the template

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -6,7 +7,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const PATHS = {
 	src: path.join(__dirname, '../src'),
 	dist: path.join(__dirname, '../dist'),
- };
+};
+
+const PAGES_DIR = `${PATHS.src}/pug/pages/`;
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'));
 
 module.exports = {
 	// BASE config 
@@ -38,7 +42,11 @@ module.exports = {
 			{
 				test: /\.js$/,
 				loader: 'babel-loader',
-				exclude: '/node_modules/',
+				exclude: '/node_modules/'
+			},
+			{
+				test: /\.pug$/,
+				loader: 'pug-loader'
 			},
 			{
 				test: /\.(png|jpg|gif|svg)$/,
@@ -84,16 +92,14 @@ module.exports = {
 		new MiniCssExtractPlugin ({
 			filename: 'css/[name].[hash].css',
 		}),
-		new HtmlWebpackPlugin ({
-			hash: false,
-			template: `${PATHS.src}/index.html`,
-			filename: 'index.html',
-			inject: true
-		}),
 		new CopyWebpackPlugin ([
 			{ from: `${PATHS.src}/img`, to: 'img' },
 			{ from: `${PATHS.src}/font`, to: 'font' },
 			{ from: `${PATHS.src}/static`, to: '' }
-		])
+		]),
+		...PAGES.map(page => new HtmlWebpackPlugin({
+			template: `${PAGES_DIR}/${page}`,
+			filename: `./${page.replace(/\.pug/, '.html')}`
+		}))
 	]
 };
